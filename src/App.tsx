61cccb238d672c9709ca2967d74144ed1899a4f6
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2024-10-25 13:14:03 +0300

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 // src/App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Scheduler from './Scheduler';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Router>
+      <Routes>
+        <Route path="/" element={<h2>Welcome to the App!</h2>} />
+        <Route path="/scheduler" element={<Scheduler />} />
+        <Route path="/scheduler-editor" element={<Scheduler />} />
+      </Routes>
+    </Router>
+  )
+)
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<h2>Welcome to the App!</h2>} />
-          <Route path="/scheduler" element={<Scheduler />} />
-          <Route path="/scheduler-editor" element={<Scheduler />} />
-        </Routes>
-      </div>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
 

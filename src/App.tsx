d293c43cbc5c4065189302c3135cff4c16438c93
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2024-10-25 13:17:17 +0300

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
 // src/App.jsx
-import { BrowserRouter as Router, Route, Routes, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Scheduler from './Scheduler';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Router>
-      <Routes>
-        <Route path="/" element={<h2>Welcome to the App!</h2>} />
-        <Route path="/scheduler" element={<Scheduler />} />
-        <Route path="/scheduler-editor" element={<Scheduler />} />
-      </Routes>
-    </Router>
-  )
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <h2>Welcome to the App!</h2>,
+      },
+      {
+        path: "scheduler",
+        element: <Scheduler />,
+      },
+      {
+        path: "scheduler-editor",
+        element: <Scheduler />,
+      },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<h2>Welcome to the App!</h2>} />
+//         <Route path="/scheduler" element={<Scheduler />} />
+//         <Route path="/scheduler-editor" element={<Scheduler />} />
+//       </Routes>
+//     </Router>
+//   )
+// )
 
 function App() {
   return (

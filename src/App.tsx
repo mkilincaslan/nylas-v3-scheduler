35a31855732e2881223a0194e303dc747f78af16
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2025-06-26 15:15:17 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,45 @@
 // src/App.jsx
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import Scheduler from './Scheduler';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Scheduler from "./Scheduler";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    children: [
-      {
-        path: "/",
-        element: <h2>Welcome to the App!</h2>,
-      },
-      {
-        path: "scheduler",
-        element: <Scheduler />,
-      },
-      {
-        path: "scheduler-editor",
-        element: <Scheduler />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     children: [
+//       {
+//         path: "/",
+//         element: <h2>Welcome to the App!</h2>,
+//       },
+//       {
+//         path: "scheduler",
+//         element: <Scheduler />,
+//       },
+//       {
+//         path: "scheduler-editor",
+//         element: <Scheduler />,
+//       },
+//     ],
+//   },
+// ]);
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<h2>Welcome to the App!</h2>} />
-//         <Route path="/scheduler" element={<Scheduler />} />
-//         <Route path="/scheduler-editor" element={<Scheduler />} />
-//       </Routes>
-//     </Router>
-//   )
-// )
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<h2>Welcome to the App!</h2>} />
+      <Route path="/scheduler" element={<Scheduler />} />
+      <Route path="/scheduler-editor" element={<Scheduler />} />
+    </>
+  )
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

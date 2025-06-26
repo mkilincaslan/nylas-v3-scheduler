e547af3578ab08768dcf2fcf6ab3d9585d6c1aa1
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2025-06-26 15:20:03 +0300

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -8,35 +8,35 @@ import {
 import "./App.css";
 import Scheduler from "./Scheduler";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     children: [
-//       {
-//         path: "/",
-//         element: <h2>Welcome to the App!</h2>,
-//       },
-//       {
-//         path: "scheduler",
-//         element: <Scheduler />,
-//       },
-//       {
-//         path: "scheduler-editor",
-//         element: <Scheduler />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <h2>Welcome to BlueCall Staging Booking App!</h2>,
+      },
+      {
+        path: "scheduler",
+        element: <Scheduler />,
+      },
+      {
+        path: "scheduler-editor",
+        element: <Scheduler />,
+      },
+    ],
+  },
+]);
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<h2>Welcome to the App!</h2>} />
-      <Route path="/scheduler" element={<Scheduler />} />
-      <Route path="/scheduler-editor" element={<Scheduler />} />
-    </>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path="/" element={<h2>Welcome to the App!</h2>} />
+//       <Route path="/scheduler" element={<Scheduler />} />
+//       <Route path="/scheduler-editor" element={<Scheduler />} />
+//     </>
+//   )
+// );
 
 function App() {
   return <RouterProvider router={router} />;
